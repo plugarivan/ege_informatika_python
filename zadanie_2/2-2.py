52ec47c34a053c2b3fa03318178aaa8b27b30445
Victorovich <ioanplugar@yandex.ru>
2021-06-18 16:20:56 +0300

    02-06

--- zadanie_2/2-2.py
+++ zadanie_2/2-2.py
@@ -3,5 +3,5 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if ((x <= w) and (y <= z) or w) == False:
+                if (((x <= y) == (z <= w)) or (x and w)) == False:
                     print(x, y, z, w)
