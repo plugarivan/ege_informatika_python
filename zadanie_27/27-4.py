30087b86db71bd48e1ed341518fbee37c2da37bd
Victorovich <ioanplugar@yandex.ru>
2021-08-08 15:47:06 +0300

    08-08

--- zadanie_27/27-4.py
+++ zadanie_27/27-4.py
@@ -5,17 +5,30 @@
 """
 with open('../files/27/s27-B.txt') as f:
     n = f.readline()
-    summa, chet, nechet = 0, 0, 0
+    summa, chet, nechet, dnech1, dnech2, dch1, dch2 = 0, 0, 0, 10001, 10001, 10001, 10001
     for x in f:
         a, b = map(int, x.split())
-        m = min(a, b)
-        summa += m
-        if m % 2 == 0:
+        mn = min(a, b)
+        mx = max(a, b)
+        summa += mn
+        if mn % 2 == 0:
             chet += 1
         else:
             nechet += 1
         if a % 2 != b % 2:
+            if mn % 2 == 0:
+                if mx - mn < dch1:
+                    dch2 = dch1
+                    dch1 = mx - mn
+                if mx - mn < dch2:
+                    dch2 = mx - mn
+            if mn % 2 != 0:
+                if mx - mn < dnech1:
+                    dnech2 = dnech1
+                    dnech1 = mx - mn
+                if mx - mn < dnech2:
+                    dnech2 = mx - mn
 
     print(summa)
-    print(summa % 2)
     print(chet, nechet)
+    print(dch1, dch2, dnech1, dnech2)
