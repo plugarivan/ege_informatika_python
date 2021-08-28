31a85665dcdbf7c02f07bf785d4df6c382689d14
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-08-28 12:28:59 +0300

    28-08

--- clear.py
+++ clear.py
@@ -1,16 +1,7 @@
-
-s = '>' + 14 * '1' + 20 * '2' + 25 * '3'
-
-while '>1' in s or '>2' in s or '>3' in s:
-    if '>1' in s:
-        s = s.replace('>1', '22>3', 1)
-    if '>2' in s:
-        s = s.replace('>2', '2>', 1)
-    if '>3' in s:
-        s = s.replace('>3', '11>2', 1)
-
-sum = 0
-for i in s:
-    if i != '>':
-        sum += int(i)
-print(sum)
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if (not(y <= (x == w)) and (z <= x)):
+                    print(x, y, z, w)
