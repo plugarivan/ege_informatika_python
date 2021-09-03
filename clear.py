e4ee625a8adb619ec6bbc0a81ae14de27609f7a2
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-09-03 13:11:03 +0300

    03-09

--- clear.py
+++
@@ -1,7 +0,0 @@
-print('x y z w')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                if (not(y <= (x == w)) and (z <= x)):
-                    print(x, y, z, w)
