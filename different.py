aefe914b9b6ac0ec2ce88b8d469466a48c8ba6c0
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-10-17 11:45:43 +0300

    17-10

--- different.py
+++ different.py
@@ -1,8 +1,28 @@
-summa = 0
-a = int(input())
-while a != 0:
-    if a % 7 == 0 and a % 10 == 2:
-        summa += a
-    a = int(input())
-print(summa)
+class ITMan:
 
+    ITMen_counter = 0
+    deleted_people = 0
+
+    def __init__(self, language, salary):
+        self.language = language
+        self.salary = salary
+        ITMan.ITMen_counter +=1
+
+    def salary_increase(self, addon):
+        self.salary += addon
+
+    def workers_counter(self):
+        print('Current IT people qty is {0}'.format(ITMan.ITMen_counter))
+
+    def __del__(self):
+        ITMan.ITMen_counter -= 1
+        ITMan.deleted_people +=1
+
+
+Buba = ITMan('Python', 1500)
+Antoha = ITMan('C++', 6000)
+Leha = ITMan('C++', 4500)
+
+del Buba
+
+print(Antoha.deleted_people)
