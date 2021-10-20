b97caa94d741a4313d70e59db0fb440e27e438ba
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-10-20 09:37:07 +0300

    20-10

--- different.py
+++ different.py
@@ -1,28 +1,12 @@
-class ITMan:
-
-    ITMen_counter = 0
-    deleted_people = 0
-
-    def __init__(self, language, salary):
-        self.language = language
-        self.salary = salary
-        ITMan.ITMen_counter +=1
-
-    def salary_increase(self, addon):
-        self.salary += addon
-
-    def workers_counter(self):
-        print('Current IT people qty is {0}'.format(ITMan.ITMen_counter))
-
-    def __del__(self):
-        ITMan.ITMen_counter -= 1
-        ITMan.deleted_people +=1
-
-
-Buba = ITMan('Python', 1500)
-Antoha = ITMan('C++', 6000)
-Leha = ITMan('C++', 4500)
-
-del Buba
-
-print(Antoha.deleted_people)
+for n in range(1, 256):
+    s = bin(n)[2:]
+    if len(s) < 8:
+        s = (8 - len(s)) * '0' + s
+    s1 = ''
+    for x in s:
+        if x == '1':
+            s1 += '0'
+        else:
+            s1 += '1'
+    if int(s1, 2) - n == 45:
+        print(n)
--- zadanie_6/6-1.py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 1000):
-    s = i
-    n = 400
-    while s - n > 0:
-        s = s - 20
-        n = n - 15
-    if s < 0:
-        print(i)
-        break
