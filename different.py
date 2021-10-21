81a5ffb493f3e872bb90513b4f32414c3aedcb1f
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-10-21 13:57:04 +0300

    21-10

--- different.py
+++ different.py
@@ -1,12 +1,22 @@
-for n in range(1, 256):
+for n in range(1, 100):
     s = bin(n)[2:]
-    if len(s) < 8:
-        s = (8 - len(s)) * '0' + s
-    s1 = ''
-    for x in s:
-        if x == '1':
-            s1 += '0'
-        else:
-            s1 += '1'
-    if int(s1, 2) - n == 45:
+    if s.count('1') == s.count('0'):
+        s += s[-1]
+    elif s.count('1') > s.count('0'):
+        s += '0'
+    else:
+        s += '1'
+    if s.count('1') == s.count('0'):
+        s += s[-1]
+    elif s.count('1') > s.count('0'):
+        s += '0'
+    else:
+        s += '1'
+    if s.count('1') == s.count('0'):
+        s += s[-1]
+    elif s.count('1') > s.count('0'):
+        s += '0'
+    else:
+        s += '1'
+    if int(s, 2) % 4 == 0:
         print(n)
