b6fe22abdef801f144f7c04b6cf639e3c48a9e5d
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-11-19 09:35:20 +0300

    19-11

--- zadanie_2/2-1.py
+++ zadanie_2/2-1.py
@@ -3,7 +3,5 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if (x and (y and z or y and (not(w)) or (not(z)) and (not(w)))) == True:
+                if ((x and y) or (y and z)) == ((x <= w) and (w <= z)):
                     print(x, y, z, w)
-
-                    #ywzx
--- zadanie_2/2-2.py
+++
@@ -1,7 +0,0 @@
-print('x y z w')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                if ((x == (not z)) <= ((x or w) == y)) == False:
-                    print(x, y, z, w)
