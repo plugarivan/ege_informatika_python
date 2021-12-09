fed2235f4156e057edc3f871cffeb5f156ff2c98
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-12-09 08:56:42 +0300

    09-12

--- zadanie_17/17-8.py
+++ zadanie_17/17-8.py
@@ -1,6 +1,26 @@
-'''
-(А. Кабанов) В файле 17-4.txt содержится последовательность целых чисел. Элементы последовательности могут принимать
-целые значения от 0 до 10 000 включительно. Рассматривается множество элементов последовательности, которые при делении
-на 13 дают остаток 7, при этом не делятся ни на 7, ни на 11. Найдите разность наибольшего и наименьшего из таких чисел
-и их количество.
-'''
+data = [int(x) for x in open('../files/17/17-243.txt')]
+
+def sumDigits( n ):
+  return sum( map(int, str(n)) )
+
+ref = sum( sumDigits(x) for x in data
+                          if x % 49 == 0 )
+print( ref )
+
+def nCond2( arr, func, func2 ):
+  return (func(arr[0]) and not func(arr[1]) and func2(arr[1]) or
+          func(arr[1]) and not func(arr[0]) and func2(arr[0]))
+
+count, mi, ma = 0, 10**10, 0
+for i, n in enumerate(data):
+   if i > 0:
+     pair = data[i-1:i+1]
+     if nCond2( pair, lambda x: x > ref,
+                      lambda x: x % 10 == 7 ):
+        # print(pair)
+        ma = max( ma, sum(pair) )
+        mi = min( mi, sum(pair) )
+        count += 1
+
+print( count, mi )
+print( count, ma )
