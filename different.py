f36e3fadaa875a1807c1bc7fcfcb5999065b66e1
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-12-15 18:34:44 +0300

    15-12

--- different.py
+++ different.py
@@ -1,7 +1,17 @@
-x = 789
-summa = 0
-while x > 0:
-    summa += x % 10
-    x //= 10
-print(summa)
+with open('../ege_informatika_python/files/26/26-s1.txt') as f:
+    data = f.readlines()
+    del data[0]
+    data = list(map(int, data))
+    b = [x for x in data if x <= 200]  # те на которые скидка не предоставляется
+    c = [x for x in data if x > 200]  # те на которые будем делать скидку
+    c.sort()
+    d = c[:len(c) // 2]  # товары на которые будет скидка
+    c = c[len(c) // 2:]
+    print(sum(b) + sum(c) + 0.7 * sum(d))
+    print(max(d))
+
+
+
+
+
 
--- zadanie_19-21/19-21-1.py
+++ zadanie_19-21/19-21-1.py
@@ -2,12 +2,16 @@
 '''
 #20 задача
 def f(x, p):
-    if x >= 42 or p > 3:
-        return p == 3 #петя выиграл вторым ходом
+    if x >= 65 or p > 3:
+        if x < 100:
+            return p == 3
+        else:
+            return p == 2
+#петя выиграл вторым ходом
     if p % 2:
-        return f(x + 1, p + 1) and f(x + 3, p + 1) and f(x * 2,  p + 1)
+        return f(x + 1, p + 1) and f(x * 3,  p + 1)
     else:
-        return f(x + 1, p + 1) or f(x + 3, p + 1) or f(x * 2,  p + 1)
+        return f(x + 1, p + 1) or f(x * 3,  p + 1)
 
 print([i for i in range(1, 100) if f(i, 0)])
 
