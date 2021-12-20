53240c9018646951f936800cb2aee97818b3e247
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2021-12-20 09:09:54 +0300

    20-12

--- different.py
+++ different.py
@@ -1,17 +1,1 @@
-with open('../ege_informatika_python/files/26/26-s1.txt') as f:
-    data = f.readlines()
-    del data[0]
-    data = list(map(int, data))
-    b = [x for x in data if x <= 200]  # те на которые скидка не предоставляется
-    c = [x for x in data if x > 200]  # те на которые будем делать скидку
-    c.sort()
-    d = c[:len(c) // 2]  # товары на которые будет скидка
-    c = c[len(c) // 2:]
-    print(sum(b) + sum(c) + 0.7 * sum(d))
-    print(max(d))
-
-
-
-
-
 
--- zadanie_19-21/19-21-1.py
+++ zadanie_19-21/19-21-1.py
@@ -18,17 +18,17 @@ print([i for i in range(1, 100) if f(i, 0)])
 
 #21 задача
 def f(x, p):
-    if x >= 42 and (p == 4 or p == 2):
+    if x >= 65 and (p == 4 or p == 2):
         return True
     else:
-        if x < 42 and p == 4:
+        if x < 65 and p == 4:
             return False
         else:
-            if x >= 42: #перебор камней
+            if x >= 65: #перебор камней
                 return False
     if p % 2 == 0:
-        return f(x + 1, p + 1) and f(x + 3, p + 1) and f(x * 2,  p + 1)
+        return f(x + 1, p + 1) and f(x * 3,  p + 1)
     else:
-        return f(x + 1, p + 1) or f(x + 3, p + 1) or f(x * 2,  p + 1)
+        return f(x + 1, p + 1) or f(x * 3,  p + 1)
 
 print(min([i for i in range(1, 100) if f(i, 0)]))
