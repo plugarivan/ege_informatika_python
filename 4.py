a99f676607ac1667413cd0a0fc8886890b7d9ddc
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-02-01 11:11:31 +0300

    01-02

--- 4.py
+++
@@ -1,4 +0,0 @@
-for m in range(2, 1000, 2):
-    for n in range(1, 1001, 2):
-        if 100000000<2**m * 7**n<300000000:
-            print(2**m * 7**n, m +n)
--- different.py
+++ different.py
@@ -1,6 +1,14 @@
-print('a b c')
-for a in range(2):
-    for b in range(2):
-        for c in range(2):
-            if (a == b or c == b):
-                print(a, b, c)
+def f(a1, a2, x):
+    return ((a1 <= x < a2) or (10 <= x <= 40)) or ((5 <= x <= 15) <= (35 <= x <= 50))
+
+s = []
+for a1 in range(-100, 100):
+    for a2 in range(-100, 100):
+        flag = True
+        for x in range(-100, 100):
+            if not(f(a1, a2, x)):
+                flag = False
+                break
+        if flag:
+            s.append(a2 - a1)
+print(min(s))
