8d92e0932121c9774cac76c4578bbe777a19384d
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-02-14 09:37:08 +0300

    14-02

--- different.py
+++ different.py
@@ -1,32 +1,11 @@
-from math import sqrt
-
-start, end = 631632, 684934
-
-def isPrime( x ):
-  if x <= 1: return False
-  d = 2
-  while d*d <= x:
-    if x % d == 0:
-      return False
-    d += 1
-  return True
-
-divs = []
-maxDiff = 0
-for i in range(start, end+1):
-  q = round(sqrt(i))
-  D = [2] + list( range(3, q+1, 2) )
-  print(D)
-  for x in D:
-    y = i // x
-    if (y - x) <= maxDiff: break
-    if i % x == 0 and isPrime(x):
-      if x != y and isPrime(y) and (y - x) > maxDiff:
-        maxDiff = y - x
-        divs = [x, y]
-      break
-
-print( divs[0], divs[1] )
+for i in range(1, 1000):
+  s = i
+  n = 200
+  while s // n >= 2:
+    s = s + 5
+    n = n + 5
+  if 99 < s < 1000:
+    print(i)
 
 
 
--- zadanie_17/17-16.py
+++ zadanie_17/17-16.py
@@ -2,4 +2,13 @@ with open('../files/17/17-10.txt') as f:
     s = [int(x) for x in f]
     numbers = []
     for i in range(1, len(s)):
-        
+        summa = s[i] + s[i - 1]
+        ch = 0
+        while summa > 0:
+            ch += summa % 7
+            summa //= 7
+        f = str(ch)
+        if f == f[::-1]:
+            fff = int(f, 7)
+            numbers.append(fff)
+print(numbers)
