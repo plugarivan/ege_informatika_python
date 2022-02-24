b6c817683e44969f7467c8e8f7accebd1ad1a42f
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-02-24 09:41:28 +0300

    24-02

--- different.py
+++ different.py
@@ -1,14 +1,2 @@
-def f(x, a):
-    return (a % 12 == 0) and ((530 % x == 0) <= ((a % x != 0) <= (170 % x != 0)))
-
-k = 0
-for a in range(1, 1001):
-    flag = True
-    for x in range(1, 1000):
-        if not(f(x, a)):
-            flag = False
-    if flag:
-        k += 1
-print(k)
 
 
