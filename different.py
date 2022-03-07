9cc139e4865029939e9bdea0f9ab12d5f655d624
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-03-07 08:09:49 +0300

    07-03

--- different.py
+++ different.py
@@ -1,1 +1,15 @@
+import time
+tt = time.time()
+for i in range(123456789, 223456790):
+    divs = set()
+    if round(i ** 0.5) == i ** 0.5:
+        for d in range(2, round(i ** 0.5) + 1):
+            if i % d == 0:
+                divs.add(d)
+                divs.add(i // d)
+                if len(divs) > 3:
+                    break
 
+    if len(divs) == 3:
+        print(i, max(divs))
+print(time.time() - tt)
---
+++ zadanie_23/23-23.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+fib = [1]*10
+for i in range(10):
+    if i > 1:
+        fib[i] = fib[i-2] + fib[i-1]
+print(fib)
+def f(x, y):
+    if x > y:
+        return 0
+    if x == y:
+        return 1
+    for c in fib:
+        if c >= x:
+            return f(x + 1, y) + f(x + 5, y) + f(x + c, y)
+
+
+print(f(1, 17))
