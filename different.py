559d4c893de3b9812d7748ac49b9e55492837380
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-03-15 20:23:11 +0300

    15-03

--- different.py
+++ different.py
@@ -1,15 +1,30 @@
-import time
-tt = time.time()
-for i in range(123456789, 223456790):
-    divs = set()
-    if round(i ** 0.5) == i ** 0.5:
-        for d in range(2, round(i ** 0.5) + 1):
-            if i % d == 0:
-                divs.add(d)
-                divs.add(i // d)
-                if len(divs) > 3:
-                    break
+#20
+def f(x, y, p):
+    if x + y <= 20 or p > 3:
+        return p == 3
+    if p % 2:
+        return f(x - 1, y, p + 1) and f(x // 2, y, p + 1) and f(x, y - 1, p + 1) and f(x, y // 2, p + 1)
+    else:
+        return f(x - 1, y, p + 1) or f(x // 2, y, p + 1) or f(x, y - 1, p + 1) or f(x, y // 2, p + 1)
 
-    if len(divs) == 3:
-        print(i, max(divs))
-print(time.time() - tt)
+print([i for i in range(100, 1, -1) if f(10, i, 0)])
+
+#21
+def f(x, y, p):
+    if x + y <= 20 and (p == 4 or p == 2):
+        return True
+    else:
+        if x + y > 20 and p == 4:
+            return False
+        else:
+            if x + y <= 20:
+                return False
+    if p % 2 == 0:
+        return f(x - 1, y, p + 1) and f(x // 2, y, p + 1) and f(x, y - 1, p + 1) and f(x, y // 2, p + 1)
+    else:
+        return f(x - 1, y, p + 1) or f(x // 2, y, p + 1) or f(x, y - 1, p + 1) or f(x, y // 2, p + 1)
+
+print([i for i in range(100, 1, -1) if f(10, i, 0)])
+
+#2324324445
+#25
--- zadanie_19-21/19-21-5.py
+++ zadanie_19-21/19-21-5.py
@@ -1,22 +1,22 @@
 #20 задача
 def f(x, p):
-    if x >= 30:
+    if x >= 48:
         return p == 3
     if p % 2:
-        return f(x + 2, p + 1) and f(x + 3, p + 1) and f(x * 2, p + 1)
+        return f(x + 1, p + 1) and f(x + 4, p + 1) and f(x * 2, p + 1)
     else:
-        return f(x + 2, p + 1) or f(x + 3, p + 1) or f(x * 2, p + 1)
+        return f(x + 1, p + 1) or f(x + 4, p + 1) or f(x * 2, p + 1)
 
-print(len([i for i in range(1, 30) if f(i, 0)]))
+print([i for i in range(1, 40) if f(i, 0)])
 #21 задача
 def f(x, p):
-    if x >= 30 and (p == 4 or p == 2):
+    if x >= 48 and (p == 4 or p == 2):
         return True
     else:
-        if x < 30 and p == 4:
+        if x < 48 and p == 4:
             return False
         else:
-            if x >= 30:
+            if x >= 48:
                 return False
     if p % 2:
         return f(x + 2, p + 1) or f(x + 3, p + 1) or f(x * 2, p + 1)
