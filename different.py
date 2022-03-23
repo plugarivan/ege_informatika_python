31b7b0fc508c3228b61709ca8c340b2b51eb3dc1
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-03-23 13:41:20 +0300

    23-03

--- different.py
+++ different.py
@@ -1,8 +1,8 @@
-letters = 'елмур'
-k = []
-for a in letters:
-    for b in letters:
-        for c in letters:
-            for d in letters:
-                k.append(a+b+c+d)
-print(k.index('леее')+1)
+def f(x, y):
+    if x > y:
+        return 0
+    elif x == y:
+        return 1
+    return f(x + 1, y) + f(int('1' + bin(x)[2:], 2), y)
+
+print(f(int('100', 2), int('110001', 2)))
