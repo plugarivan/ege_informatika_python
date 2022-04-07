2157338a374f279f771380c3fb927edd6e07eaa4
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-04-07 12:05:58 +0300

    07-04

--- stream.py
+++ stream.py
@@ -1,10 +1,15 @@
-for a in range(1, 60):
-    for b in range(1, 60):
-        for c in range(1, 60):
-            s = '0' + a * '1' + b * '2' + c * '3'
-            while '01' in s or '02' in s or '03' in s:
-                s = s.replace('01', '2302', 1)
-                s = s.replace('02', '10', 1)
-                s = s.replace('03', '201', 1)
-            if s.count('1') == 51 and s.count('2') == 29 and s.count('3') == 23:
-                print(c)
+i = 800001
+k = 0
+while k != 5:
+    divs = set()
+    for d in range(2, round(i** 0.5) + 1):
+        if i % d == 0:
+            divs.add(d)
+            divs.add(i // d)
+    if len(divs) != 0:
+        if (max(divs) + min(divs)) % 138 == 0:
+            print(i, (max(divs) + min(divs)))
+            k += 1
+    i += 1
+
+
