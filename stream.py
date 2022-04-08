e05a93a423e0fa0f236165e0edc0877a5795aa3e
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-04-08 08:07:36 +0300

    08-04

--- stream.py
+++ stream.py
@@ -1,15 +1,17 @@
-i = 800001
-k = 0
-while k != 5:
-    divs = set()
-    for d in range(2, round(i** 0.5) + 1):
-        if i % d == 0:
-            divs.add(d)
-            divs.add(i // d)
-    if len(divs) != 0:
-        if (max(divs) + min(divs)) % 138 == 0:
-            print(i, (max(divs) + min(divs)))
-            k += 1
-    i += 1
+'''
+2 ** n * p ** 4
+'''
 
+def prost(n):
+    for d in range(2, round(n ** 0.5) + 1):
+        if n % d == 0:
+            return False
+    return True
 
+for n in range(105000000, 115000001):
+    x = n
+    while x % 2 == 0:
+        x //= 2
+    sqrt4 = round(x ** 0.25)
+    if prost(sqrt4) and sqrt4 ** 4 == x:
+        print(n, x)
