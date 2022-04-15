654bb870df300c77ff6bdf6d5118fb77d55457fd
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-04-15 11:02:50 +0300

    15-04

--- stream.py
+++ stream.py
@@ -1,3 +1,18 @@
-for x in range(5, 36):
-    if int('13', x) * int('31', x) == int('423', x):
-        print(x)
+with open('./files/26/26-50.txt') as f:
+    n = int(f.readline())
+    s = [int(i) for i in f]
+    s.sort()
+    sred = []
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            summa = s[i] + s[j]
+            if summa % 2 == 0:
+                sred.append(summa // 2)
+    sred.sort()
+    numbers = [x for x in sred if s[n // 2 - 1] < x < s[3 * n // 4]]
+print(len(numbers), min(numbers))
+
+
+
+
+
